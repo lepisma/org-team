cc20bc259044a7c82367eabc659d29db970d7f0a
Abhinav Tushar <abhinav@lepisma.xyz>
2021-01-25 17:23:58 +0530

    basic notes tracker

--- org-team.el
+++ org-team.el
@@ -4,7 +4,7 @@
 
 ;; Author: Abhinav Tushar <abhinav@lepisma.xyz>
 ;; Version: 0.0.1
-;; Package-Requires: ((emacs "27") (org "9.4"))
+;; Package-Requires: ((emacs "27") (f "0.20.0") (helm "3.7.0") (org "9.4"))
 ;; URL: https://github.com/lepisma/org-team
 
 ;;; Commentary:
@@ -29,7 +29,61 @@
 
 ;;; Code:
 
+(require 'cl-lib)
+(require 'dash)
+(require 'eieio)
+(require 'f)
+(require 'helm)
 (require 'org)
+
+(defcustom org-team-dir nil
+  "Directory with team files."
+  :type 'directory)
+
+(defclass org-team-person ()
+  ((directory :initarg :directory))
+  "A person in org-team")
+
+(cl-defmethod org-team-person-file ((p org-team-person))
+  "Return index file for person P if present."
+  (let ((file-path (f-join (oref p :directory) "index.org")))
+    (when (f-exists-p file-path)
+      file-path)))
+
+(defun org--file-title (file)
+  "Return title for an org file"
+  (with-temp-buffer
+    (insert-file-contents file nil nil 1000)
+    (goto-char (point-min))
+    (when (re-search-forward "^#\\+TITLE:\\(.*\\)$")
+      (s-trim (match-string-no-properties 1)))))
+
+(cl-defmethod org-team-person-name ((p org-team-person))
+  "Return name of the person."
+  (let ((index-file (org-team-person-file p)))
+    (when index-file
+      (org--file-title index-file))))
+
+(cl-defmethod org-team-person-open-file ((p org-team-person))
+  (find-file (org-team-person-file p)))
+
+(defun org-team-list-people ()
+  "Return a list of people found in ORG-TEAM-DIR."
+  (unless org-team-dir
+    (error "`org-team-dir' not set."))
+  (->> (f-directories org-team-dir)
+     (-map (lambda (d) (make-instance 'org-team-person :directory d)))
+     (-remove (lambda (p) (null (org-team-person-file p))))))
+
+(defun org-team ()
+  "Main function to interact with people in team."
+  (interactive)
+  (let ((people (org-team-list-people)))
+    (helm :sources (helm-build-sync-source "Team member"
+                     :candidates (-map (lambda (p) (cons (org-team-person-name p) p)) people)
+                     :action `(("Open notes" . org-team-person-open-file)))
+          :buffer "*helm org team*"
+          :prompt "Name: ")))
 
 (provide 'org-team)
 
